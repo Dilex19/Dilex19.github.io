cf990e430c981a62a375ff881b112f9efc53446d
Dilex19 <123111927+Dilex19@users.noreply.github.com>
2023-01-20 15:46:07 -0300

    intento 2

---
+++ cv.js
@@ -0,0 +1,7 @@
+document.getElementById('boton').addEventListener('click', function (){
+    document.getElementById('navbar-nav-menu').classList.toggle('navbar-nav_visble');
+})
+
+document.getElementById('boton-info').addEventListener('click', function (){
+    document.getElementById('inf-contacto').classList.toggle('inf-contacto_visible');
+})
