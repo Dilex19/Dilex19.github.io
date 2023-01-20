ee9dceb6bacf610196fe1868adee96de67c1600b
Dilex19 <123111927+Dilex19@users.noreply.github.com>
2023-01-20 15:45:24 -0300

    intento fallido

--- Programacion/cv.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('boton').addEventListener('click', function (){
-    document.getElementById('navbar-nav-menu').classList.toggle('navbar-nav_visble');
-})
-
-document.getElementById('boton-info').addEventListener('click', function (){
-    document.getElementById('inf-contacto').classList.toggle('inf-contacto_visible');
-})
